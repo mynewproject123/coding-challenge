50831dd2a8fc49e3bf240adce0466c1904b2e7ae
mynewproject123 <umakondapureddy32@gmail.com>
2025-09-01 23:49:24 -0400

    Define input variables for reusability and configuration flexibility

---
+++ input.tf
@@ -0,0 +1,36 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "vpc_cidr_block" {
+  type = string
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "ssh_cidr" {
+  type = string
+}
