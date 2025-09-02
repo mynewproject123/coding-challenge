618fa19756ddf1c6b18c98d8b263072556b93be9
mynewproject123 <umakondapureddy32@gmail.com>
2025-09-01 23:59:11 -0400

    Implement EC2 module to launch instances with security groups and key pair

---
+++ modules/ec2/main.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "web" {
+  vpc_id = var.vpc_id
+  name   = "${var.ec2_name}-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "${var.ec2_name}-sg" }
+}
+
+resource "aws_instance" "this" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  key_name                    = var.key_name
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  associate_public_ip_address = true
+  tags = { Name = var.ec2_name }
+}
